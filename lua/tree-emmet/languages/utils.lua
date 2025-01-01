e73dc3719e51fadc300b6f4ad7e5e3285d7ee9db
dorage-san <baloonflower554@gmail.com>
2025-01-01 12:04:55 +0900

    -

--- lua/tree-emmet/languages/utils.lua
+++ lua/tree-emmet/languages/utils.lua
@@ -4,10 +4,6 @@ local M = {}
 ---@field is_element_node fun(): boolean
 ---@field is_opening_element_node fun(element_node: TSNode): boolean
 ---@field is_closing_element_node fun(element_node: TSNode): boolean
----@field get_identifier fun(element_node: TSNode): string
-
----comment
----@param opts LangauageOptions
 ---@return LangauageOptions
 M.create_lang = function(opts)
   return opts
