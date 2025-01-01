800f0f13e65130a4a4d48a8730997ed4c2a71eb9
dorage-san <baloonflower554@gmail.com>
2025-01-01 12:04:45 +0900

    ref: add language type

--- lua/tree-emmet/languages/utils.lua
+++ lua/tree-emmet/languages/utils.lua
@@ -1,13 +1,14 @@
 local M = {}
 
--- isElementNode
--- isOpeningElementNode
--- isClosingElementNode
--- getIdentifier
+---@class LangauageOptions
+---@field is_element_node fun(): boolean
+---@field is_opening_element_node fun(element_node: TSNode): boolean
+---@field is_closing_element_node fun(element_node: TSNode): boolean
+---@field get_identifier fun(element_node: TSNode): string
 
 ---comment
----@param opts any
----@return any
+---@param opts LangauageOptions
+---@return LangauageOptions
 M.create_lang = function(opts)
   return opts
 end
